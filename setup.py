b739997d59330688b6bb0e385a913646e5196ab0
Kaarel Ratas <kaarel.ratas@gmail.com>
2019-06-07 10:34:17 +0300

    Improved packaging.

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from codecs import open
 
 from setuptools import setup, find_packages
 
+here = path.abspath(path.dirname(__file__))
+
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
@@ -25,4 +27,14 @@ setup(
     include_package_data=True, # Use MANIFEST.in for data files
     install_requires=['six'],
     tests_require=['nose'],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Logging',
+        'Topic :: Utilities',
+    ]
 )
